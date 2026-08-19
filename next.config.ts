07dad9e796d6eb3c9d8b5a6dce8ca84fc68eb596
Kevin Zimmerman <4733573+kczimm@users.noreply.github.com>
2026-08-18 20:42:06 -0500

    Expose app on LAN (0.0.0.0) on port 3100 with allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Allow the dev server to be reached from other devices on the LAN.
+     Next blocks non-localhost hosts in dev by default. Add any origin
+     (or map a steady hostname to your machine's IP) that should be allowed. */
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://localhost:3100",
+    "http://192.168.1.149:3000",
+    "http://192.168.1.149:3100",
+  ],
 };
 
 export default nextConfig;
